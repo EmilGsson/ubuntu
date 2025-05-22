a88f26ebe8e5f6820889af2894fcdd6108f55096
EmilGsson <emilgoransson1@hotmail.com>
2025-05-22 17:12:15 +0200

    Lade till configfiler för DHCP, DNS, logrotate och script

---
+++ config/loggit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DATUM=$(date +"%Y-%m-%d_%H-%M")
+REPO_DIR="$HOME/ubuntu/loggar"
+
+mkdir -p "$REPO_DIR/$DATUM"
+cp /var/log/syslog.* "$REPO_DIR/$DATUM"
+
+cd $HOME/ubuntu
+git add .
+git commit -m "Backup loggar $DATUM"
+git push
